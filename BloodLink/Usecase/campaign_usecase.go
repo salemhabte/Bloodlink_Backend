0e2cd297d237025c3889f24155d2b4b3f2eba414
nebiyattakele <nebiyattakele23@gmail.com>
2026-03-08 20:52:16 +0300

    feat: add donor campaign APIs, fix repository

--- BloodLink/Usecase/campaign_usecase.go
+++ BloodLink/Usecase/campaign_usecase.go
@@ -2,33 +2,37 @@ package Usecase
 
 import (
 	"bloodlink/Domain"
-	
 )
 
 type CampaignUsecase struct {
-    Repo Domain.ICampaignRepository
+	Repo Domain.ICampaignRepository
 }
 
 func NewCampaignUsecase(repo Domain.ICampaignRepository) *CampaignUsecase {
-    return &CampaignUsecase{Repo: repo}
+	return &CampaignUsecase{Repo: repo}
 }
 
 func (u *CampaignUsecase) CreateCampaign(campaign *Domain.Campaign) error {
-    return u.Repo.CreateCampaign(campaign)
+	return u.Repo.CreateCampaign(campaign)
 }
 
 func (u *CampaignUsecase) GetAllCampaigns() ([]Domain.Campaign, error) {
-    return u.Repo.GetAllCampaigns()
+	return u.Repo.GetAllCampaigns()
 }
 
 func (u *CampaignUsecase) GetCampaignByID(id string) (*Domain.Campaign, error) {
-    return u.Repo.GetCampaignByID(id)
+	return u.Repo.GetCampaignByID(id)
 }
 
 func (u *CampaignUsecase) UpdateCampaign(campaign *Domain.Campaign) error {
-    return u.Repo.UpdateCampaign(campaign)
+	return u.Repo.UpdateCampaign(campaign)
 }
 
 func (u *CampaignUsecase) DeleteCampaign(id string) error {
-    return u.Repo.DeleteCampaign(id)
+	return u.Repo.DeleteCampaign(id)
+}
+
+// Donor Feature
+func (u *CampaignUsecase) GetCampaignsByLocation(location string) ([]Domain.Campaign, error) {
+	return u.Repo.GetCampaignsByLocation(location)
 }
